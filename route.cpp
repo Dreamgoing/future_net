5a87220fa5d96e1050118268ff692dfeec881573
Dreamgoing <wangruoxuan@wangruoxuandeMacBook-Pro.local>
2016-03-21 22:05:25 +0800

    dfs
    dfs search
    Don’t run correctly

--- route.cpp
+++ route.cpp
@@ -12,7 +12,7 @@
 #include <map>
 #include <__hash_table>
 using namespace std;
-//#define DEBUG_SHOW_PATH
+#define DEBUG_SHOW_PATH
 //#define DEBUG_SHOW_STFN
 //#define DEBUG_SHOW_DIST
 //你要完成的功能总入口
@@ -39,6 +39,7 @@ vector<int> vertexes;
 vector<int> result;
 pair<int,int> stfn;
 bool visit[max_n];
+vector<int> resPath;
 struct pairHash{
     template <typename T1,typename T2>
             size_t operator()(const std::pair<T1,T2> &pr)const {
@@ -47,12 +48,14 @@ struct pairHash{
     }
 };
 unordered_map<pair<int,int>,vector<int>,pairHash > path; //init the unorder_map
+unordered_map<pair<int,int>,int,pairHash> hashPath;
 void printPath(const pair<int,int> p);
 void solve_input_topo(char *topo[5000],int edge_num){
     /*
      * solve the case that there are more than two edge between two vertexes*/
     memset(mapp,inf, sizeof(mapp));
     memset(dist,inf, sizeof(dist));
+    hashPath.clear();
     path.clear();
     int tmp[4];
     for(int i = 0;i<max_n;i++){
@@ -73,13 +76,15 @@ void solve_input_topo(char *topo[5000],int edge_num){
         }
         graph[tmp[1]].push_back(Edge(tmp[2],tmp[3],tmp[0]));
 
+
         path[make_pair(tmp[1],tmp[2])].push_back(tmp[0]);
+        hashPath[make_pair(tmp[1],tmp[2])] = tmp[0];
 #ifdef DEBUG_SHOW_PATH
 
-        cout<<path[make_pair(tmp[1],tmp[2])].size()<<" size"<<endl;
+       // cout<<path[make_pair(tmp[1],tmp[2])].size()<<" size"<<endl;
         printPath(make_pair(tmp[1],tmp[2]));
-        cout<<"vector  "<<path[make_pair(tmp[1],tmp[2])][0]<<" "<<endl;
-        cout<<tmp[0]<<"--> num"<<endl;
+        //cout<<"vector  "<<path[make_pair(tmp[1],tmp[2])][0]<<" "<<endl;
+       // cout<<tmp[0]<<"--> num"<<endl;
 #endif
         mapp[tmp[1]][tmp[2]] = tmp[3];
         dist[tmp[1]][tmp[2]] = tmp[3];
@@ -148,13 +153,15 @@ void printPath(const pair<int,int> p) {
 
 void unionPath(const pair<int,int> a,const pair<int,int> b){
     pair<int,int> res = make_pair(a.first,b.second);
-    copy(path[a].begin(),path[a].end(),back_inserter(path[res]));
-    path[res].assign(path[b].begin(),path[b].end());
+    path[res].reserve(path[a].size()+path[b].size());
+    path[res].assign(path[a].begin(),path[a].end());
+    //printPath(res);
+    path[res].insert(path[res].end(),path[b].begin(),path[b].end());
+    //printPath(res);
 }
 //find the shortest path in set vs obtain source vertex
-void prim(const vector<Edge>graph[],vector<int> &vs,int n){
+void prim(const vector<Edge>graph[],vector<int> vs,int n){
     path.clear();
-    vs.push_back(stfn.second);
     for(int i = 0;i<n;i++){
        dist[i][i] = 0;
     }
@@ -177,7 +184,7 @@ void prim(const vector<Edge>graph[],vector<int> &vs,int n){
                      * coding
                      * */
                     unionPath(make_pair(from,k),make_pair(k,to));
-                    printPath(make_pair(from,to));
+                  //  printPath(make_pair(from,to));
 
                 }
             }
@@ -186,6 +193,85 @@ void prim(const vector<Edge>graph[],vector<int> &vs,int n){
 
 
 
+}
+
+void shortest_path(const vector<Edge>graph[],vector<int> vs,int n)
+{
+  //  path.clear();
+    for(int i = 0;i<n;i++){
+        dist[i][i] = 0;
+    }
+    bool isVs[max_n];
+    vs.push_back(stfn.first);
+    memset(isVs,0, sizeof(isVs));
+    for(vector<int>::iterator it = vs.begin();it!=vs.end();++it){
+        isVs[*it] = true;
+    }
+    cout<<isVs[stfn.first]<<" "<<isVs[stfn.second]<<endl;
+    for(int k = 0;k<n;k++){
+        if(k==stfn.first||k==stfn.second||!isVs[k]){
+            continue;
+        }
+        for(int i = 0;i<vs.size();i++){
+            for(int j = 0;j<vs.size();j++){
+                if(vs[j] == stfn.first){
+                    continue;
+                }
+                int from = vs[i];
+                int to = vs[j];
+                if(dist[from][k]+dist[k][to]<dist[from][to]){
+                    dist[from][to] = dist[from][k]+dist[k][to];
+                    //path<make_pair<i,j>,vector<int> > = path<make_pair<i,k>,vector<int> > + path<make_pair<i,k>, vector<int> >
+                    //if the sourceID and the destinationID in the path of i and j solve it
+                    /*
+                     * coding
+                     * */
+                    unionPath(make_pair(from,k),make_pair(k,to));
+                    //printPath(make_pair(from,to));
+
+                }
+            }
+        }
+    }
+
+}
+int mincost = inf;
+
+void printVector(vector<int> s);
+void dfs(int s,int cost,int num){
+    if(num == vertexes.size()){
+        if(dist[s][stfn.second]<inf&&cost+dist[s][stfn.second]<mincost){
+
+            resPath.push_back(hashPath[make_pair(s,stfn.second)]);
+            cout<<"______________________________________"<<endl;
+            cout<<"cost "<<cost+dist[s][stfn.second]<<endl;
+
+            mincost = cost + dist[s][stfn.second];
+            for(int i = 0;i<resPath.size();i++){
+                cout<<resPath[i]<<" ";
+            }
+            cout<<'\n';
+            resPath.pop_back();
+        }
+        return;
+    } else{
+        for(vector<int>::iterator it = vertexes.begin();it!=vertexes.end();++it){
+            if(!visit[*it]){
+                if(dist[s][*it]<inf){
+                    visit[*it] = true;
+                    cost += dist[s][*it];
+                    resPath.insert(resPath.end(),path[make_pair(s,*it)].begin(),path[make_pair(s,*it)].end());
+                    printVector(resPath);
+                    dfs(*it,cost,num+1);
+                    for(int i = 0;i<path[make_pair(s,*it)].size();i++){
+                        resPath.pop_back();
+                    }
+                    cost -= dist[s][*it];
+                    visit[*it] = false;
+                }
+            }
+        }
+    }
 }
 void dijkstra(const vector<Edge> graph[],int s){
     int di[max_n];
@@ -232,7 +318,11 @@ void solve_search(){
      * */
     memset(visit,0, sizeof(visit));
     result.clear();
-    prim(graph,vertexes,max_vertexnum);
+    //prim(graph,vertexes,max_vertexnum);
+    resPath.clear();
+    shortest_path(graph,vertexes,max_vertexnum);
+    memset(visit,0,sizeof(visit));
+    dfs(stfn.first,0,0);
 
 }
 void branchAndBound(){
@@ -245,14 +335,28 @@ void branchAndBound(){
      * a lower bound is formulated for the given node
      * construction of solution tree*/
 }
+void test(){
+    unionPath(make_pair(2,3),make_pair(3,1));
+    printPath(make_pair(2,1));
+}
+
+
+void printVector(vector<int> s) {
+    for(vector<int>::iterator it = s.begin();it!=s.end();++it){
+        cout<<*it<<" -> ";
+    }
+    cout<<'\n';
+}
+
 void search_route(char *topo[5000], int edge_num, char *demand) {
     /*unsigned short result[] = {2, 6, 3};//示例中的一个解
     for (int i = 0; i < 3; i++)
         record_result(result[i]);*/
     solve_input_topo(topo,edge_num);
     solve_input_demand(demand);
-    printMap(max_vertexnum);
+    //printMap(max_vertexnum);
     solve_search();
+    //test();
 
 
 }
